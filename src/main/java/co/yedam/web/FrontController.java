533f12cb9890d0a35d55ccfcf5d175d4e2c080e3
tmd4314 <tmd4314@naver.com>
2025-04-16 22:46:18 +0900

    login

--- src/main/java/co/yedam/web/FrontController.java
+++ src/main/java/co/yedam/web/FrontController.java
@@ -10,8 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.yedam.control.LoginControl;
-import com.yedam.control.LoginFormControl;
 
 import co.yedam.common.Control;
 
