4098d99a8137346a04919e14840ca198073b8607
tmd4314 <tmd4314@naver.com>
2025-04-16 09:04:03 +0900

    메인 시작 부분 수정

--- src/main/java/co/yedam/web/MainControl.java
+++ src/main/java/co/yedam/web/MainControl.java
@@ -1,57 +1,24 @@
 package co.yedam.web;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import co.yedam.common.Control;
-import co.yedam.service.ProductService;
-import co.yedam.service.ProductServiceImpl;
+
 
 public class MainControl implements Control {
 
 	@Override
 	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		Date now = new Date();
-		Date dueDate = new Date();
-		dueDate.setHours(17);
-		dueDate.setMinutes(50);
-		dueDate.setSeconds(0);
+		HttpSession session = req.getSession();
+		String logId = (String) session.getAttribute("logId");
 
-		String remainTimeStr = "";
-
-		try {
-
-			long diff = dueDate.getTime() - now.getTime();
-			long h = diff / (1000 * 60 * 60);
-			diff -= (h * (1000 * 60 * 60));
-			long m = diff / (1000 * 60);
-			diff -= (m * (1000 * 60));
-			long s = diff / 1000;
-			remainTimeStr = h + "시간 " //
-					+ ("0" + m).substring(("0" + m).length() - 2) + "분 "//
-					+ ("0" + s).substring(("0" + s).length() - 2) + "초 남음.";
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		ProductService svc = new ProductServiceImpl();
-		List<Map<String, Object>> cheers = svc.cheeringMessage();
-		List<Map<String, Object>> hints = svc.hintMessage(remainTimeStr);
-
-		req.setAttribute("message", cheers);
-		req.setAttribute("hint", hints);
-
-		req.getRequestDispatcher("index.jsp")//
+		req.getRequestDispatcher("product/index.tiles")//
 				.forward(req, resp);
 	}
 
